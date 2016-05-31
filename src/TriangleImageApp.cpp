a6706a23256dc2c12263912cbc5a06eabb0a0ad4
Will Gallia <wgallia@gmail.com>
2016-05-31 12:46:08 +0100

    half triangles

--- src/TriangleImageApp.cpp
+++ src/TriangleImageApp.cpp
@@ -6,8 +6,6 @@ using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-//using BaseTileRef = std::shared_ptr<BaseTile>;
-
 class BaseTile : public Shape2d {
 public:
     virtual void setColourFromChannel(Channel &channel) = 0;
@@ -15,7 +13,6 @@ public:
     virtual string getId() = 0;
     virtual vec2 getCenter() const { return mCenter; }
     
-protected:
     vec2 mCenter;
 };
 
@@ -85,7 +82,6 @@ public:
         
         output->transform(transform);
         
-//        output->mCenter = output->calcBoundingBox().getCenter();
         vec2 point(0.0f);
         for (auto &p : output->getContour(0).getPoints()) {
             point+= p;
@@ -106,30 +102,23 @@ public:
             pixels = PixelCache::pixelCache.at(getId());
         }
         catch (std::out_of_range e) {
-//            addToPixelCache(mSideLength);
             float radius = mSideLength / std::sqrt(3.0f);
             auto up = create(vec2(0), mSideLength, true);
             PixelCache::pixelCache.emplace(up->getId(), PixelCache::create(*up.get(), radius));
             auto down = create(vec2(0), mSideLength, false);
             PixelCache::pixelCache.emplace(down->getId(), PixelCache::create(*down.get(), radius));
-//            pixelCache.emplace(make_pair(sideLength, DOWN), PixelCache::createWithOrientation(sideLength, DOWN));
-//            pixelCache.emplace(make_pair(sideLength, UP), PixelCache::createWithOrientation(sideLength, UP));
 
             pixels = PixelCache::pixelCache.at(getId());
         }
         
         auto *channelData = channel.getData();
         
-//        auto channelAreaIter = channel.getIter(pixels->mBoundingBox);
         uint32_t pixelSum = 0;
         float nPixels = 0;
         for (auto &offset : pixels->mInsidePositions) {
 
             ivec2 pos = ivec2(mCenter.x, mCenter.y) - offset;
             if (channel.getBounds().contains(pos)) {
-//            cout << pos << endl;
-//                cout << channelData[pos.x + pos.y * channel.getRowBytes()] << endl;
-//                cout << endl;
                 pixelSum += uint32_t(channelData[pos.x + pos.y * channel.getRowBytes()]);
                 nPixels++;
             }
@@ -139,15 +128,7 @@ public:
         float v = pixelSum / 255.0f / nPixels;
         float d = 6;
         v = std::floor(v * d) / d;
-//        if (v > 0.25) v = 1.0;
-//        else v = 0.0;
-        
-//        cout << pixelSum  << endl;
-//        cout <<pixels->mInsidePositions.size() << endl;
-//        cout << v << endl;
-//        cout << n << endl;
-//        cout << endl;
-//        v = 1.0f;
+
         mColour = Color(v, v, v);
     }
     
@@ -157,17 +138,208 @@ public:
     
     vec2 getCenter() const override { return mCenter; }
     ivec2 getICenter() { return ivec2(mCenter.x, mCenter.y); }
-//    vec2 mOffset;
-    
-    
-//    using PixelCacheRef = shared_ptr<struct Triangle::PixelCache>;
-    
     
     Orientation mOrientation;
-    vec2 mCenter;
     float mSideLength;
     Color mColour;
 };
+
+class HalvedTriangles : public Triangle {
+public:
+    
+    struct HalfTriangle : public Triangle {
+        string getId() override {
+            char id[50];
+            sprintf(id, "HTri-%2.2f-%s-%c-%d", mSideLength, mOrientation == UP ? "u" : "d", mSide, mRotation);
+            return string(id);
+        }
+        
+        static shared_ptr<HalfTriangle> create(vec2 p, float sideLength, Orientation orientation, int rotation, char side) {
+            
+            auto output = make_shared<HalfTriangle>();
+            
+            vec2 center(sideLength * 0.5, sideLength / std::sqrt(3.0f) / 2.0f);
+            vec2 a(0.0f);
+            vec2 b(sideLength, 0);
+            vec2 c(sideLength * 0.5, sideLength * 0.5f * sqrt(3.0f));
+            
+            if (rotation == 120 && side == 'r') {
+                output->moveTo(a);
+                output->lineTo(center);
+                output->lineTo((a + c) * 0.5f);
+                output->close();
+            }
+            else if (rotation == 120 && side == 'l') {
+                output->moveTo(a);
+                output->lineTo((a + b) * 0.5f);
+                output->lineTo(center);
+                output->close();
+            }
+            else if (rotation == 60 && side == 'r') {
+                output->moveTo(b);
+                output->lineTo(center);
+                output->lineTo((b + a) * 0.5f);
+                output->close();
+            }
+            else if (rotation == 60 && side == 'l') {
+                output->moveTo(b);
+                output->lineTo((b + c) * 0.5f);
+                output->lineTo(center);
+                output->close();
+            }
+            else if (rotation == 0 && side == 'r') {
+                output->moveTo(c);
+                output->lineTo(center);
+                output->lineTo((c + b) * 0.5f);
+                output->close();
+            }
+            else if (rotation == 0 && side == 'l') {
+                output->moveTo(c);
+                output->lineTo((c + a) * 0.5f);
+                output->lineTo(center);
+                output->close();
+            }
+            else {
+                assert(1 == 0);
+            }
+            
+            mat3 transform = glm::translate(mat3(1.0), p);
+            
+            if (orientation == UP) {
+                transform = glm::rotate(transform, 3.14159f / 3.0f);
+                
+            }
+            
+            output->transform(transform);
+            
+//            vec2 point(0.0f);
+//            for (auto &p : output->getContour(0).getPoints()) {
+//                point+= p;
+//            }
+//            output->mCenter = point / 3.0f;
+            output->mCenter = (a + b + c) / 3.0f;
+            if (orientation == UP) {
+                output->mCenter = glm::rotate(output->mCenter, 3.14159f / 3.0f);
+            }
+            output->mCenter+= p;
+            
+            output->mOrientation = orientation;
+            output->mSideLength = sideLength;
+            output->mRotation = rotation;
+            output->mSide = side;
+            
+            //        output->mColour = Color(p.x / 600.0f, p.y / 400.0f, 0.5);
+            output->mColour = Color(0, 0, 0);
+            
+            return output;
+        }
+        
+        vector<Colorf> getColors() override {
+            return vector<Colorf>(3, mColour);
+        }
+
+        
+        char mSide;
+        int mRotation;
+
+    };
+    
+    string getId() override {
+        char id[50];
+        sprintf(id, "HTris-%2.2f", mSideLength);
+        return string(id);
+    }
+    
+    static shared_ptr<HalvedTriangles> create(vec2 p, float sideLength, bool down=true) {
+        
+        auto output = make_shared<HalvedTriangles>();
+        
+        vector<int> angles = { 0, 60, 120 };
+        vector<char> sides = { 'l', 'r' };
+        for (auto angle : angles) {
+            for (auto side : sides) {
+                auto hf = HalfTriangle::create(p, sideLength, down ? DOWN : UP, angle, side);
+                output->append(*hf.get());
+                output->mHalves.push_back(hf);
+            }
+        }
+        
+        
+        output->mOrientation = down ? DOWN : UP;
+        output->mSideLength = sideLength;
+        output->mOffset = p;
+        output->mColour = Color(0, 1, 0);
+        
+        return output;
+    }
+    
+    void setColourFromChannel(Channel &channel) override {
+        shared_ptr<PixelCache> pixels;
+
+        mColours.clear();
+        
+        for (auto half : mHalves) {
+        
+            try {
+                pixels = PixelCache::pixelCache.at(half->getId());
+            }
+            catch (std::out_of_range e) {
+                float radius = mSideLength / 2.0f;
+                auto freshHalf = HalfTriangle::create(vec2(0), half->mSideLength, half->mOrientation, half->mRotation, half->mSide);
+                
+                PixelCache::pixelCache.emplace(freshHalf->getId(), PixelCache::create(*freshHalf.get(), radius));
+                
+                assert(freshHalf->getId() == half->getId());
+                
+                pixels = PixelCache::pixelCache.at(half->getId());
+            }
+            
+            auto *channelData = channel.getData();
+            
+            uint32_t pixelSum = 0;
+            float nPixels = 0;
+            for (auto &offset : pixels->mInsidePositions) {
+                
+                ivec2 pos = ivec2(mOffset.x, mOffset.y) + offset;
+                if (channel.getBounds().contains(pos)) {
+                    pixelSum += uint32_t(channelData[pos.x + pos.y * channel.getRowBytes()]);
+                    nPixels++;
+                }
+                
+            }
+            
+            float v = pixelSum / 255.0f / nPixels;
+            float d = 8;
+            v = std::floor(v * d) / d;
+            v+= 0.125;
+
+            half->mColour = Color(v, v, v);
+
+        }
+        
+    }
+    
+    vector<Colorf> getColors() override {
+        vector<Colorf> output;
+        for (auto half : mHalves) {
+            auto halfColours = half->getColors();
+            output.insert(output.end(), halfColours.begin(), halfColours.end());
+        }
+        return output;
+    }
+    
+    vec2 getCenter() const override { return mCenter; }
+    ivec2 getICenter() { return ivec2(mCenter.x, mCenter.y); }
+    
+    Orientation mOrientation;
+    float mSideLength;
+    Color mColour;
+    vector<Colorf> mColours;
+    vec2 mOffset;
+    
+    vector<shared_ptr<HalfTriangle>> mHalves;
+};
+
 
 
 class TriGrid {
@@ -195,11 +367,11 @@ public:
                 }
                 
                 bool doDown = (y) % 2;
-                auto downTri = Triangle::create(p, mSideLength, doDown);
+                auto downTri = HalvedTriangles::create(p, mSideLength, doDown);
                 mTriangles.push_back(downTri);
                 
                 if ((y % 2 == 0 && i != mNumTris.x - 1) || (y % 2 == 1 && i != 0)) {
-                    auto upTri = Triangle::create(p, mSideLength, !doDown);
+                    auto upTri = HalvedTriangles::create(p, mSideLength, !doDown);
                     mTriangles.push_back(upTri);
                 }
 
@@ -230,7 +402,7 @@ public:
     float mSideLength;
     ivec2 mNumTris;
     
-    vector<TriangleRef> mTriangles;
+    vector<shared_ptr<HalvedTriangles>> mTriangles;
 };
 
 class TriangleImageApp : public App {
@@ -254,44 +426,21 @@ class TriangleImageApp : public App {
 
 void TriangleImageApp::setup()
 {
-    
-//    auto tri = Triangle::create(vec2(0.0f), 12);
-//    auto pc = Triangle::PixelCache::createWithOrientation(12, Triangle::UP);
-//    map<pair<int, int>, bool> points;
-//    for (auto p : pc->mInsidePositions) {
-//        points.emplace(make_pair(int(p.x), int(p.y)), true);
-////        cout << (vec2(p.x, p.y) +tri->getCenter()) << endl;
-//    }
-//    
-//    for (int y = pc->mBoundingBox.getY1(); y < pc->mBoundingBox.getY2(); y++) {
-//        for (int x = pc->mBoundingBox.getX1(); x < pc->mBoundingBox.getX2(); x++) {
-//            if (points.count(make_pair(x, y)) > 0) {
-//                cout << "1";
-//            }
-//            else {
-//                cout << "0";
-//            }
-//        }
-//        cout << endl;
-//    }
-//    for (auto &p : pc->mInsidePositions) cout << p;
-//    cout << endl;
-//    quit();
-    
-    
-//    mSurface = Surface::create(loadAsset("cat.png"));
-//    Surface surface(loadAsset("cat.png"))
-    mChannel = Channel::create(loadImage(loadAsset("couple.jpg")));
+
+//    mChannel = Channel::create(loadImage(loadAsset("couple.jpg")));
+    mChannel = Channel::create(loadImage(loadAsset("giulia_marco.jpg")));
     mTexture = gl::Texture::create(*mChannel.get());
     setWindowSize(mChannel->getSize());
     
     float start = getElapsedSeconds();
     
-    mTriGrid.setup(getWindowSize(), 50);
+    mTriGrid.setup(getWindowSize(), 32);
     mTriGrid.colourTriangles(*mChannel.get());
     mTriGrid.populateMesh();
     
     cout << (getElapsedSeconds() - start) << endl;
+    
+    cout << (mTriGrid.mTriangles.size() * 6) << endl;
 
 }
 
@@ -306,26 +455,51 @@ void TriangleImageApp::update()
 void TriangleImageApp::draw()
 {
 	gl::clear( Color( 1, 1, 1 ) );
-//    gl::color(0, 0, 0);
-//
+
+    gl::color(1, 1, 1);
     gl::disableWireframe();
-    gl::draw(mTexture);
+//    gl::draw(mTexture);
 
     gl::pushMatrices();
-//    gl::translate(vec2(20, 20));
-    gl::enableWireframe();
+
     gl::draw(*mTriGrid.mTriMesh.get());
     
+
+    auto &cols = mTriGrid.mTriMesh->getBufferColors();
+    vector<float> colsCopy(cols.begin(), cols.end());
+    cols.clear();
+    for (size_t i = 0; i < colsCopy.size(); ++i) {
+        cols.push_back(0.45f);
+    }
+    gl::enableWireframe();
+
+    gl::draw(*mTriGrid.mTriMesh.get());
+    
+    cols = colsCopy;
+
+    
+//    
+//    gl::color(1, 0, 0);
+//    for (auto ht : mTriGrid.mTriangles) {
+//        for (auto half : ht->mHalves) {
+//            gl::drawSolidCircle(half->mCenter, 2);
+//        }
+//    }
+//
+//    gl::color(0, 1, 0);
+//    for (auto p :PixelCache::pixelCache.at("HTri-50.00-u-l-0")->mInsidePositions) {
+////        auto c = mTriGrid.mTriangles[0]->mHalves[0]->mCenter;
+//        auto c = mTriGrid.mTriangles[0]->mOffset;
+////        cout << mTriGrid.mTriangles[0]->mHalves[0]->getId() << endl;
+//        p = ivec2(c.x, c.y) + p;
+//        gl::drawSolidRect(Rectf(p.x, p.y, p.x+1, p.y+1));
+//    }
+//    
+//    gl::color(0, 0, 1);
+//    gl::drawSolidCircle(mTriGrid.mTriangles[0]->mHalves[0]->mCenter, 2);
+//    
     gl::popMatrices();
     
-    
-
-
-    
-//    gl::draw
-    
-//    auto tri = makeTriangle(vec2(getMousePos() - getWindowPos()), 50);
-//    gl::draw(tri);
 }
 
 CINDER_APP( TriangleImageApp, RendererGl )
